06650839fc6bc02150663a2f395b17c3e71dbbb4
Norsi47 <briggsnorsi@yahoo.com>
2022-06-20 23:02:27 -0400

    adding random comment

--- app/src/main/java/com/example/sql_prac/MainActivity.java
+++ app/src/main/java/com/example/sql_prac/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
+//aaaaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
